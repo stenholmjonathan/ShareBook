132784c005ee27d69f20f8abef201674a2c305be
samy <samykiiskinen@gmail.com>
2023-12-03 17:41:33 +0100

    working with try catch

---
+++ ShareBook/Exceptions/BlogPostNotFoundException.cs
@@ -0,0 +1,12 @@
+﻿namespace ShareBook.Exceptions
+{
+    public class BlogPostNotFoundException : Exception
+    {
+        public BlogPostNotFoundException() { }
+
+        //public ProfileNotFoundException(string message) : base(message) { }
+
+        //public ProfileNotFoundException(string message, Exception innerException) : base(message, innerException) { }     
+
+    }
+}
---
+++ ShareBook/Exceptions/ProfileNotFoundException.cs
@@ -0,0 +1,12 @@
+﻿namespace ShareBook.Exceptions
+{
+    public class ProfileNotFoundException : Exception
+    {
+        public ProfileNotFoundException() { }
+
+        //public ProfileNotFoundException(string message) : base(message) { }
+
+        //public ProfileNotFoundException(string message, Exception innerException) : base(message, innerException) { }     
+
+    }
+}
