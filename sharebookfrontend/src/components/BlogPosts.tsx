825cb54babd8fcb96b398a51f7c40395387176f4
J0nki <kiiskinenjony@gmail.com>
2023-12-07 21:15:12 +0100

    create models for frontend

--- sharebookfrontend/src/components/BlogPosts.tsx
+++ sharebookfrontend/src/components/BlogPosts.tsx
@@ -1,23 +1,32 @@
 import React from "react";
 import axios from "axios";
+import BlogPost from "../models/BlogPost";
 
 export default class BlogPosts extends React.Component {
   state = {
-    blogposts: [],
+    blogposts: [] as BlogPost[], // Explicitly initialize with an empty array of Profile type
   };
 
   componentDidMount() {
-    axios.get(`https://localhost:7179/BlogPost/GetAllBlogPosts`).then((res) => {
-      const blogposts = res.data;
-      this.setState({ blogposts });
-    });
+    axios
+      .get<BlogPost[]>("https://localhost:7179/BlogPost/GetAllBlogPosts")
+      .then((res) => {
+        const blogposts = res.data;
+        this.setState({ blogposts });
+      })
+      .catch((error) => {
+        console.error("Error fetching profiles:", error);
+      });
   }
 
   render() {
     return (
       <ul>
         {this.state.blogposts.map((blogposts) => (
-          <li key={blogposts["id"]}>{blogposts["subject"]}</li>
+          <li key={blogposts.id}>
+            ID: {blogposts.id}, Subject: {blogposts.subject}, Message:
+            {blogposts.message}
+          </li>
         ))}
       </ul>
     );
--- sharebookfrontend/src/components/Profiles.tsx
+++ sharebookfrontend/src/components/Profiles.tsx
@@ -1,23 +1,32 @@
 import React from "react";
 import axios from "axios";
+import Profile from "../models/Profile";
 
 export default class Profiles extends React.Component {
   state = {
-    profiles: [],
+    profiles: [] as Profile[], // Explicitly initialize with an empty array of Profile type
   };
 
   componentDidMount() {
-    axios.get(`https://localhost:7179/Profile/GetAll`).then((res) => {
-      const profiles = res.data;
-      this.setState({ profiles });
-    });
+    axios
+      .get<Profile[]>("https://localhost:7179/Profile/GetAll")
+      .then((res) => {
+        const profiles = res.data;
+        this.setState({ profiles });
+      })
+      .catch((error) => {
+        console.error("Error fetching profiles:", error);
+      });
   }
 
   render() {
     return (
       <ul>
-        {this.state.profiles.map((profiles) => (
-          <li key={profiles["id"]}>{profiles["name"]}</li>
+        {this.state.profiles.map((profile) => (
+          <li key={profile.id}>
+            ID: {profile.id}, Name: {profile.name}, Description:{" "}
+            {profile.description}
+          </li>
         ))}
       </ul>
     );
