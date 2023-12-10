b2eb29ab459afed81c52c549e0afc32f4adc4ed0
J0nki <kiiskinenjony@gmail.com>
2023-12-10 09:50:17 +0100

    added Chakra UI

--- sharebookfrontend/src/App.tsx
+++ sharebookfrontend/src/App.tsx
@@ -1,12 +1,30 @@
+import { Grid, GridItem, Show } from "@chakra-ui/react";
 import Profiles from "./components/Profiles.tsx";
 import BlogPosts from "./components/BlogPosts.tsx";
+import NavBar from "./components/NavBar.tsx";
 
 function App() {
   return (
-    <div>
+    <Grid
+      templateAreas={{
+        base: `"nav main"`,
+        lg: `"nav nav" "aside main"`,
+      }}
+    >
+      <GridItem area="nav" bg="lightblue">
+        <NavBar />
+      </GridItem>
+      <Show above="lg">
+        <GridItem area="aside" bg="pink">
+          Aside
+        </GridItem>
+      </Show>
+      <GridItem area="main" bg="#3182CE">
+        Main
+      </GridItem>
       <Profiles />
       <BlogPosts />
-    </div>
+    </Grid>
   );
 }
 
