ec4f01214600ba1a25e65dea7b2050403c748dec
stenholmjonathan <stenholmjonathan@gmail.com>
2024-01-10 21:55:38 +0100

    clean up branch from login things

--- sharebookfrontend/src/App.tsx
+++ sharebookfrontend/src/App.tsx
@@ -1,19 +1,11 @@
 import { Grid, GridItem, Show } from "@chakra-ui/react";
 import { Route, Routes } from "react-router-dom";
-import Home from "./Pages/Home.tsx";
-import Login from "./Pages/Login.tsx";
-import Profiles from "./components/Profiles.tsx";
 import BlogPosts from "./components/BlogPosts.tsx";
-import BlogPost_2 from "./components/BlogPost_2.tsx";
 import NavBar from "./components/NavBar.tsx";
 
 function App() {
   return (
     <>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/" element={<Login />} />
-      </Routes>
       <Grid
         templateAreas={{
           base: `"nav" "main"`,
--- sharebookfrontend/src/services/api-client.ts
+++
@@ -1,6 +0,0 @@
-import axios from "axios";
-
-// create an axios instance with a custom configuration
-export default axios.create({
-    baseURL: 'https://localhost:7179'
-}) 
