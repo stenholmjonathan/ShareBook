009e90f7accb0d35b908ba4ae908fb1d89a52863
stenholmjonathan <stenholmjonathan@gmail.com>
2024-02-03 21:27:31 +0100

    test

--- sharebookfrontend/src/App.tsx
+++ sharebookfrontend/src/App.tsx
@@ -1,25 +1,41 @@
-import { Grid, GridItem, Show } from "@chakra-ui/react";
-import BlogPosts from "./components/BlogPosts.tsx";
-import NavBar from "./components/NavBar.tsx";
+// App.tsx
+import React, { useState, useEffect } from 'react';
+import LoginPage from './pages/LoginPage';
+import BlogPosts from './components/BlogPosts.tsx';
+import { Grid, GridItem } from '@chakra-ui/react';
+import NavBar from './components/NavBar.tsx';
 
-function App() {
+const App: React.FC = () => {
+  const [user, setUser] = useState<string | null>(null);
+
+  useEffect(() => {
+    // Check if the username is already in localStorage on component mount
+    const storedUsername = localStorage.getItem('username');
+    if (storedUsername) {
+      setUser(storedUsername);
+    }
+  }, []);
+
+  const handleLogin = (username: string) => {
+    localStorage.setItem('username', username);
+    setUser(username);
+  };
+
   return (
-    <>
-      <Grid
-        templateAreas={{
-          base: `"nav" "main"`,
-          lg: `"nav" "main"`,
-        }}
-      >
-        <GridItem area="nav" bg="lightblue">
-          <NavBar />
-        </GridItem>
-        <GridItem area="main" bg="#3182CE">
-          <BlogPosts />
-        </GridItem>
-      </Grid>
-    </>
+    <Grid
+      templateAreas={{
+        base: `"nav" "main"`,
+        lg: `"nav" "main"`,
+      }}
+    >
+      <GridItem area="nav" bg="lightblue">
+        <NavBar />
+      </GridItem>
+      <GridItem>
+        {user ? <BlogPosts /> : <LoginPage onLogin={handleLogin} />}
+      </GridItem>
+    </Grid>
   );
-}
+};
 
 export default App;
